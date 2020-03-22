f0cabe6c6742e0e28c859bcf4a331b2c6dadd283
sanjibbehera <51856169+sanjibbehera@users.noreply.github.com>
2020-03-22 16:05:36 +0530

    update
    Now the EC2 instance will only run btw 9AM to 16PM and only on weekdays.

--- python3-start_stop-ec2_instances/handler.py
+++ python3-start_stop-ec2_instances/handler.py
@@ -1,22 +1,28 @@
 import boto3
+import datetime
 
 ec2 = boto3.client('ec2')
+weekno = datetime.datetime.today().weekday()
 
 
 def start_ec2_instances(event, context):
     ec2_instances = get_all_stopped_ec2_ids()
-    response = ec2.start_instances(
-        InstanceIds=ec2_instances,
-        DryRun=False
-    )
+    response = ""
+    if weekno<5:
+        response = ec2.start_instances(
+            InstanceIds=ec2_instances,
+            DryRun=False
+        )
     return response
 
 def stop_ec2_instances(event, context):
     ec2_instances = get_all_started_ec2_ids()
-    response = ec2.stop_instances(
-        InstanceIds=ec2_instances,
-        DryRun=False
-    )
+    response = ""
+    if weekno<5:
+        response = ec2.stop_instances(
+            InstanceIds=ec2_instances,
+            DryRun=False
+        )
     return response
 
 
